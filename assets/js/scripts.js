a4d22c8227325858448e5083f199927b2afba93b
Anthony <anthony.ringeisen.dev@gmail.com>
2021-09-16 16:27:56 +0200

    data

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+// RAPPEL DES TYPES DE DONNEES
+
+let string = "Anthony";
+let number = 24;
+let boolean = true;
+let maVariable; // UNDEFINED
+
+// TABLEAUX
+
+let array = [
+    "Bordeaux",
+    "Toulouse",
+    "Nantes"
+];
+
+console.log(array[1]); // [Vise l'index visé]
+
