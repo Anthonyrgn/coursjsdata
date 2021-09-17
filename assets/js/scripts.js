b43c929deae3551cd70b2145553d6262f98977e3
Anthony <anthony.ringeisen.dev@gmail.com>
2021-09-17 09:12:16 +0200

    objects and arrays

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,11 +7,55 @@ let maVariable; // UNDEFINED
 
 // TABLEAUX
 
-let array = [
-    "Bordeaux",
-    "Toulouse",
-    "Nantes"
+let array = ["Bordeaux", "Toulouse", "Nantes"];
+
+// console.log(array[0][3]); // [Vise l'index visé]
+
+let array2 = [
+  "Bordeaux",
+  24,
+  true,
+  [1, 2],
+  {
+    prenom: "Denis",
+  },
+];
+// console.log(array2 [4].prenom);
+
+let object = {
+  pseudo: "Anthony",
+  age: 24,
+  techno: ["Javascript", "React", "NodeJs"],
+  admin: false,
+};
+// object.adresse = "22 rue du code"; // ajouter un élément
+
+// // console.log(object);
+
+let data = [
+  {
+    pseudo: "Denis",
+    age: 33,
+    techno: ["Javascript", "React", "NodeJs"],
+    admin: false,
+  },
+  {
+    pseudo: "Samia",
+    age: 24,
+    techno: ["Javascript", "React", "NodeJs"],
+    admin: false,
+  },
+  {
+    pseudo: "Nicolas",
+    age: 42,
+    techno: ["PHP", "React", "NodeJs"],
+    admin: true,
+  },
 ];
 
-console.log(array[1]); // [Vise l'index visé]
+// console.log(data[2].pseudo);
+
+// -----------------------------
+// LES STRUCTURES DE CONTROLE
+
 
